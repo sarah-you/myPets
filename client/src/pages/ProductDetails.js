f6882310d513726d41979c0e9d6027c3a3bdc678
Sarah You <yousarahh@gmail.com>
2023-06-22 18:01:38 +0000

    removed the notes

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -15,7 +15,6 @@ export default function ProductDetails() {
   const [userId, setUserId] = useState();
   const navigate = useNavigate();
 
-  // render product data via api call
   useEffect(() => {
     async function loadProduct(productId) {
       try {
@@ -31,7 +30,6 @@ export default function ProductDetails() {
     loadProduct(productId);
   }, [productId]);
 
-  // store userdata in local storage to access userId
   useEffect(() => {
     const stringData = localStorage.getItem('userInput');
     if (stringData !== null) {
@@ -41,7 +39,6 @@ export default function ProductDetails() {
     return;
   }, []);
 
-  // logic while waiting for product data to load & its error handling
   if (isLoading) return <div>Loading...</div>;
   if (error) {
     return (
@@ -52,7 +49,6 @@ export default function ProductDetails() {
   }
   if (!product) return null;
 
-  //destructured product data to use front end
   const {
     productName,
     ratings,
@@ -70,7 +66,6 @@ export default function ProductDetails() {
     detail5,
   } = product;
 
-  // add to cart api call that sends request to server to add this product info to cart db
   async function handleCart() {
     try {
       await addtoCart(productId, userId);
@@ -82,7 +77,6 @@ export default function ProductDetails() {
     }
   }
 
-  // add to savedlist api call that sends request to server to add this product info to wishlist db
   async function handleSaveItem() {
     try {
       await addtoWishList(productId, userId);
